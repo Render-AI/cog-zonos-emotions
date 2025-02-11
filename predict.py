988769c459f1400971f5bec939a484f3bcd6eb30
💔 <cto@govpro.ai>
2025-02-10 20:44:23 -0500

    trainable

--- predict.py
+++ predict.py
@@ -4,18 +4,26 @@ import torchaudio
 from pget import pget_manifest
 from zonos.model import Zonos
 from zonos.conditioning import make_cond_dict
-
+from typing import Optional
 
 class Predictor(BasePredictor):
 
-  def setup(self):
+  def setup(self, weights: Optional[Path] = None):
     pget_manifest('manifest.pget')
     self.model = Zonos.from_local("./zonos-v0.1/config.json", "./zonos-v0.1/model.safetensors", device="cuda")
     self.model.bfloat16()
+    if weights is not None and weights.name == "weights": weights = None; # fixme
+    if weights:
+      self.speaker_embedding = torch.load(weights)
+    else:
+      self.speaker_embedding = None
 
-  def predict(self, text: str = Input(description="Text to speak"), audio: Path = Input(description="Audio with voice to mimic")) -> Path:
+  def predict(self, text: str = Input(description="Text to speak!"), audio: Path = Input(description="(Optional) Audio with voice to mimic", default=None)) -> Path:
     wav, sampling_rate = torchaudio.load(audio)
-    spk_embedding = self.model.make_speaker_embedding(wav, sampling_rate)
+    if self.speaker_embedding is None:
+      spk_embedding = self.model.make_speaker_embedding(wav, sampling_rate)
+    else:
+      spk_embedding = self.speaker_embedding
 
     torch.manual_seed(421)
 
---
+++ train.py
@@ -0,0 +1,16 @@
+from cog import Input, Path, BaseModel
+import torch
+import torchaudio
+from zonos.model import Zonos
+
+
+class TrainingOutput(BaseModel):
+  weights: Path
+
+def train(audio: Path = Input(description="Audio with voice to mimic")) -> TrainingOutput:
+  model = Zonos.from_pretrained("Zyphra/Zonos-v0.1-transformer", device="cuda")
+  model.bfloat16()
+  wav, sampling_rate = torchaudio.load(audio)
+  spk_embedding = model.make_speaker_embedding(wav, sampling_rate)
+  torch.save(spk_embedding, "spk_embedding.pt")g
+  return TrainingOutput(weights=Path("spk_embedding.pt"))
